e5b8e68f4da374b80eed79db73f2e405fdea64a4
SaiRyker <daniichernyshov0@gmail.com>
2023-08-28 11:57:50 +0300

    calculating game

---
+++ src/games/calcLogic.js
@@ -0,0 +1,31 @@
+import getRandomNum from '../getRandomNum.js';
+import gameProcess from '../index.js';
+
+const instructions = 'What is the result of the expression?';
+
+const getResult = (min, max, selectedOperator) => {
+  switch (selectedOperator) {
+    case '+':
+      return max + min;
+    case '-':
+      return max - min;
+    case '*':
+      return max * min;
+    default:
+      return null;
+  }
+};
+
+const startGame = () => {
+  const operators = ['+', '-', '*'];
+  const [min, max] = [getRandomNum(0, 100), getRandomNum(0, 100)].sort();
+  const selectedOperator = operators[getRandomNum(0, 3)];
+  const question = `${max} ${selectedOperator} ${min}`;
+  const result = getResult(min, max, selectedOperator);
+
+  return [question, result];
+};
+
+const mainProcess = () => gameProcess(instructions, startGame);
+
+export default mainProcess;
