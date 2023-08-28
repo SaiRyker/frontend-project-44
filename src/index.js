5400b959e1eee562641efb43dc2035ae1ff097a6
SaiRyker <daniichernyshov0@gmail.com>
2023-08-28 11:57:28 +0300

    created main games logic

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+import greeting from './cli.js';
+
+const gameProcess = (instructions, gameFunc) => {
+  const name = greeting();
+  const amountOfRounds = 3;
+  let count = 0;
+  while (count !== amountOfRounds) {
+    const [question, corRes] = gameFunc();
+    console.log(question);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (String(answer) === String(corRes)) {
+      count += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${corRes}"\nLet's try again,${name}`);
+      break;
+    }
+  }
+
+  if (count === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+export default gameProcess;
