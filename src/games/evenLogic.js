5f6024034c4a2433ee984f3a2a22715a78405792
SaiRyker <daniichernyshov0@gmail.com>
2023-08-28 11:56:54 +0300

    update evenLogic

--- src/games/evenLogic.js
+++ src/games/evenLogic.js
@@ -1,41 +1,26 @@
-import readlineSync from 'readline-sync';
-import greeting from '../cli.js';
+import gameProcess from '../index.js';
+import getRandomNum from '../getRandomNum.js';
 
+const instructions = 'Answer "yes" if the number is even, otherwise answer "no".';
 // eslint-disable-next-line consistent-return
 const isEven = (number) => {
   if (number % 2 === 0) {
     return 'yes';
   }
+
   if (number % 2 !== 0) {
     return 'no';
   }
 };
 
 const startGame = () => {
-  const name = greeting();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let count = 0;
-  while (count !== 3) {
-    const number = Math.floor(Math.random() * (100 - 1) + 1);
+  const number = getRandomNum(0, 100);
+  const question = `Question ${number}`;
+  const even = isEven(number);
 
-    console.log(`Question ${number}`);
-
-    const answer = readlineSync.question('Your answer: ');
-
-    const even = isEven(number);
-
-    if (even === answer) {
-      console.log('Correct!');
-      count += 1;
-    } else if ((even !== answer) || (answer !== 'yes' && answer !== 'no')) {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${even}"\nLet's try again,${name}`);
-      break;
-    }
-  }
-
-  if (count === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
+  return [question, even];
 };
 
-export default startGame;
+const mainProcess = () => gameProcess(instructions, startGame);
+
+export default mainProcess;
