912fd5d26edf870e800c9c0a844c7164692a959f
SaiRyker <daniichernyshov0@gmail.com>
2023-09-04 21:05:47 +0300

    update progLogic

--- src/games/progressionLogic.js
+++ src/games/progressionLogic.js
@@ -5,7 +5,7 @@ const instructions = 'What number is missing in the progression?';
 
 const makeProgressionArray = () => {
   const temp = [];
-  const lengthOfArr = getRandomNum(5, 10);
+  const lengthOfArr = getRandomNum(6, 10);
   const difference = getRandomNum(1, 30);
   const emptyIndex = getRandomNum(0, lengthOfArr - 1);
   let firstNumber = getRandomNum(0, 100);
