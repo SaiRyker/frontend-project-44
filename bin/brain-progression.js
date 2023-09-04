afeee22d053d3188f18406c7805013940d9eb9ef
SaiRyker <daniichernyshov0@gmail.com>
2023-09-04 20:10:47 +0300

    create new game

---
+++ bin/brain-progression.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import mainProcess from '../src/games/progressionLogic.js';
+
+mainProcess();
---
+++ src/games/progressionLogic.js
@@ -0,0 +1,30 @@
+import gameProcess from '../index.js';
+import getRandomNum from '../getRandomNum.js';
+
+const instructions = 'What number is missing in the progression?';
+
+const makeProgressionArray = () => {
+  const temp = [];
+  const lengthOfArr = getRandomNum(5, 10);
+  const difference = getRandomNum(1, 30);
+  const emptyIndex = getRandomNum(0, lengthOfArr - 1);
+  let firstNumber = getRandomNum(0, 100);
+
+  for (let i = 0; i < lengthOfArr; i += 1) {
+    temp[i] = firstNumber;
+    firstNumber += difference;
+  }
+  const emptyNumber = temp[emptyIndex];
+  temp[emptyIndex] = '..';
+
+  return [temp, emptyNumber];
+};
+
+const startGame = () => {
+  const [array, result] = makeProgressionArray();
+  const question = `Question: ${array}`;
+  return [question, result];
+};
+const mainProcess = () => gameProcess(instructions, startGame);
+
+export default mainProcess;
